Fix quantile computation bug

l0_layers.py
@@ -36,6 +36,9 @@ class LatencyTable(nn.Module):
             raise ValueError("Only bilerp supported for now.")
         key_columns.sort()
         data = []
+        measures = measures.groupby(key_columns).quantile(0.75).reset_index()
+        row_size = 0
+        col_size = 0
         for _, row in measures.iterrows():
             if len(key_columns) == 1:
                 i = 1
@@ -44,11 +47,14 @@ class LatencyTable(nn.Module):
             j = row[key_columns[-1]]
             i = (int(i) + bias) // multiplier
             j = (int(j) + bias) // multiplier
+            row_size = max(row_size, i)
+            col_size = max(col_size, j)
             lat = float(row["measurements"])
             data.append((i, j, lat))
         self.bias = bias
         self.multiplier = multiplier
-        row_size, col_size = i + 1, j + 1
+        row_size += 1
+        col_size += 1
         self.register_buffer("table", torch.empty(row_size, col_size))
         for i, j, lat in data:
             self.table[i, j] = lat

train_wide_resnet.py
@@ -53,6 +53,7 @@ parser.add_argument('--lamba', type=float, default=0.001,
                     help='Coefficient for the L0 term.')
 parser.add_argument("--tie_all_weights", action="store_true")
 parser.add_argument('--beta_ema', type=float, default=0.99)
+parser.add_argument('--target_latency', type=float, default=0)
 parser.add_argument('--lr_decay_ratio', type=float, default=0.2)
 parser.add_argument('--dataset', choices=['c10', 'c100'], default='c10')
 parser.add_argument('--local_rep', action='store_true')
@@ -69,7 +70,7 @@ total_steps = 0
 exp_flops, exp_l0 = [], []
 
 
-def gather_latencies(net, tie_all_weights=False, n_samples=1000):
+def gather_latencies(net, tie_all_weights=False, n_samples=1000, target=0):
     l0_modules = find_l0_modules(net)
     samples = []
     if tie_all_weights:
@@ -110,7 +111,7 @@ def gather_latencies(net, tie_all_weights=False, n_samples=1000):
                 lp += sd1.log_prob(mask).sum(1)
                 measurements += layer.conv1.lat_table(torch.cuda.LongTensor([layer.conv1.in_channels]), s1)
                 measurements += layer.conv2.lat_table(s1, torch.cuda.LongTensor([layer.conv2.out_channels]))
-    return (lp * measurements).mean()
+    return (lp * (measurements - target).clamp(min=0)).mean()
 
 def main():
     global args, best_prec1, writer, time_acc, total_steps, exp_flops, exp_l0
@@ -208,7 +209,7 @@ def main():
         else:
             reg = model.regularization() if not args.multi_gpu else model.module.regularization()
             if args.lat_lambda:
-                lat_loss = args.lat_lambda * gather_latencies(model, args.tie_all_weights)
+                lat_loss = args.lat_lambda * gather_latencies(model, args.tie_all_weights, target=args.target_latency)
             else:
                 lat_loss = 0
             # print(lat_loss.item(), reg.item())
@@ -256,7 +257,7 @@ def main():
             if model.module.beta_ema > 0:
                 state['avg_params'] = model.module.avg_param
                 state['steps_ema'] = model.module.steps_ema
-        save_checkpoint(state, is_best, args.name)
+        save_checkpoint(state, is_best, args.name, epoch=epoch)
     print('Best error: ', best_prec1)
     if args.tensorboard:
         writer.close()

utils.py
@@ -95,7 +95,7 @@ def adjust_learning_rate(optimizer, epoch, lr=0.1, lr_decay_ratio=0.1, epoch_dro
         param_group['lr'] = lr
 
 
-def save_checkpoint(state, is_best, name, filename='checkpoint.pth.tar'):
+def save_checkpoint(state, is_best, name, filename='checkpoint.pth.tar', epoch=0):
     """Saves checkpoint to disk"""
     directory = "runs/%s/" % name
     if not os.path.exists(directory):
@@ -104,3 +104,5 @@ def save_checkpoint(state, is_best, name, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
     if is_best:
         shutil.copyfile(filename, 'runs/%s/' % name + 'model_best.pth.tar')
+    if epoch in (179, 189):
+        shutil.copyfile(filename, 'runs/%s/' % name + f'model_{epoch}.pth.tar')